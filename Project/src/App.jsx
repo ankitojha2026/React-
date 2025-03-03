58c8bd9e913e38c081fd795ae79aaf9a671c4b96
ankitojha2026 <ojhaankit064@gmail.com>
2025-03-03 23:42:27 +0530

    todo with react

---
+++ Project/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return <center class="container"><h1>Todo App</h1>
+  
+  <div class="container text-center">
+  <div class="row">
+    <div class="col-4"> col1 </div>
+    <div class="col-4"> col2 </div>
+    <div class="col-2"> col3 </div>
+  </div>
+</div>
+  
+  
+  </center>
+}
+
+export default App;
--- Project/src/main.jsx
+++ Project/src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-// this import file is uesd inside because of we need to connect to bootstrape
-import 'bootstrap/dist/css/bootstrap.min.css'
 
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
