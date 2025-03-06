e5362800a1db044aa48140556a855042c0de1ce6
ankitojha2026 <ojhaankit064@gmail.com>
2025-03-06 23:38:42 +0530

    Bharat clock by using React

---
+++ BharatClock/src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Header from './component/Header'
+function App() {
+
+  return (
+  <>
+  <Header></Header>
+  </>
+  )
+}
+
+export default App
---
+++ ToDoApp/src/App.jsx
@@ -0,0 +1,15 @@
+import AppName from "./components/AppName";
+import TodoEntery from "./components/TodoEntery";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css';
+function App() {
+
+  return (
+<center>
+<AppName/> 
+<TodoEntery/>
+</center>
+   );
+}
+
+export default App;
