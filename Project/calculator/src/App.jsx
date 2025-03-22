336f31820d2c9520e3a78a30ddbb0dd37d8eb96f
ankitojha2026 <ojhaankit064@gmail.com>
2025-03-22 17:03:09 +0530

    Ready ButtonContainer Component

--- Project/calculator/src/App.jsx
+++ Project/calculator/src/App.jsx
@@ -1,33 +1,17 @@
 
 import style from './App.module.css';
 import Display from './component/Display';
+import ButtonConatiners from './component/ButtonConatiners';
 
 function App() {
  return (
         <center>
             <div className={style.calculator}>
             <Display></Display>
+            <ButtonConatiners></ButtonConatiners>
+        
 
-<div className={style.buttonContainer}>
-    <button>C</button>
-    <button>1</button>
-    <button>2</button>
-    <button>+</button>
-    <button>3</button>
-    <button>4</button>
-    <button>-</button>
-    <button>5</button>
-    <button>6</button>
-    <button>*</button>
-    <button>7</button>
-    <button>8</button>
-    <button>/</button>
-    <button>9</button>
-    <button>.</button>
-    <button>0</button>
-    <button>=</button>
-    <button>%</button>
-</div>
+
 
 </div>
         </center>
---
+++ Project/calculator/src/component/ButtonConatiners.jsx
@@ -0,0 +1,29 @@
+import style from './ButtonContainer.module.css';
+const ButtonConatiners = () => 
+{
+return (
+
+<div className={style.buttonContainer}>
+    <button>C</button>
+    <button>1</button>
+    <button>2</button>
+    <button>+</button>
+    <button>3</button>
+    <button>4</button>
+    <button>-</button>
+    <button>5</button>
+    <button>6</button>
+    <button>*</button>
+    <button>7</button>
+    <button>8</button>
+    <button>/</button>
+    <button>9</button>
+    <button>.</button>
+    <button>0</button>
+    <button>=</button>
+    <button>%</button>
+</div>
+
+);
+}
+export default ButtonConatiners;
