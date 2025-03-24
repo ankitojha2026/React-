0645884d39b15004b94d933d14e336787bc16204
ankitojha2026 <ojhaankit064@gmail.com>
2025-03-24 21:34:39 +0530

    new update inside this project

--- Project/FoodCard/src/App.jsx
+++ Project/FoodCard/src/App.jsx
@@ -1,14 +1,47 @@
+import { useState } from 'react';
 import './App.css'
 
 import Container from './componants/Container';
 function App() {
 
   const food=['daal' , 'chalwal' ,'sabj' ,'pakawadi' , 'salad']
+
+  let newFoodItem='Kadhi Chawal Pakaudi';
+
+
+  const showChange =(event) =>
+  {
+  setStateMethod(event.target.value);
+  }
+
+
+
+const handleBuyButton = (item) =>
+{
+console.log(`${item}`)
+}
+
+let textState = useState('Kadhi Chawal Pakaudi');
+let textStateVal=textState[0];
+let setStateMethod = textState[1];
+
+
  return (
  <>
- 
+ <center>
+  
  <Container>
   <h1 className='Head' >Healthy Food</h1>
+
+  <input className="foodInput" type="text" placeholder='Enter Food Name'
+  
+  onChange= {(event) => showChange(event) }
+  
+  
+  />
+
+  <p>{textStateVal}</p>
+  
 
   <ul className='list-group'>
     {
@@ -17,10 +50,14 @@ function App() {
         
         <button
         
-        onClick={() => console.log(`${item}`)
+        onClick={() => handleBuyButton(item) 
       }
         
         >Buy</button>
+
+
+        
+
          </li>
       
       ))
@@ -30,6 +67,7 @@ function App() {
 
    
   </Container>
+ </center>
  </>
  )
 
